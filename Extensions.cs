e754da274f6c10187a2857f2bfc4e9b08219d437
JackLite <jackkdlite@gmail.com>
2021-12-16 21:54:27 +0300

    Fix activate/deactivate module

---
+++ Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Leopotam.Ecs;
+
+namespace EcsCore
+{
+    public static class Extensions
+    {
+        public static void ActivateModule<T>(this EcsWorld world) where T : EcsModule
+        {
+            world.NewEntity().Replace(new EcsModuleActivationSignal { ModuleType = typeof(T) });
+        }
+        
+        public static void DeactivateModule<T>(this EcsWorld world) where T : EcsModule
+        {
+            world.NewEntity().Replace(new EcsModuleDeactivationSignal { ModuleType = typeof(T) });
+        }
+    }
+}
